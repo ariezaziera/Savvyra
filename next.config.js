d7ba3c37483f38a7290195aec78ca5e708b83bc4
ariezaaziera <ariezaziera@gmail.com>
2026-05-06 01:22:56 +0800

    CHANGE ICON

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withPWA = require("next-pwa")({
+  dest: "public",
   register: true,
   skipWaiting: true,
 });
 
-const nextConfig = {
+module.exports = withPWA({
   reactStrictMode: true,
-};
-
-module.exports = withPWA(nextConfig);
+});
