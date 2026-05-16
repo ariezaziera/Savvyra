3a78c38687f4a7d4269231ceb0156a89fddcb126
Arieza Aziera <152407762+ariezaziera@users.noreply.github.com>
2026-05-16 09:18:53 +0800

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -8,7 +8,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     id: "/",
     display: "standalone",
-    theme_color: "#3F2B7A",
+    theme_color: "#5D3FD3",
     background_color: "#453284",
     icons: [
       {
